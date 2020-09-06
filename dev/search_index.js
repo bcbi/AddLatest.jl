c093328f5b89f5dfe8562352894842b0d742b96a
Documenter.jl <documenter@juliadocs.github.io>
2020-09-06 04:50:06 +0000

    build based on 7976bb2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AddLatest","category":"page"},{"location":"#AddLatest","page":"Home","title":"AddLatest","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AddLatest]","category":"page"}]
+}
