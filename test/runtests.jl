dba52d64151d702353813d53b752ef19e11aca79
Dilum Aluthge <dilum@aluthge.com>
2020-09-06 04:05:25 -0400

    Run the doctests during the test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using AddLatest
 using Test
 
+import Documenter
+
 @testset "AddLatest.jl" begin
-    # Write your tests here.
+    @testset "Unit tests" begin
+    end
+
+    @testset "Integration tests" begin
+    end
+
+    @testset "Doctests" begin
+        Documenter.doctest(AddLatest)
+    end
 end
