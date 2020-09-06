c5557cb72378b575a420c74b9583a56883e1b44d
Dilum Aluthge <dilum@aluthge.com>
2020-09-06 09:12:56 +0000

    Add some more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,8 @@ import Pkg
 import UUIDs
 
 @testset "AddLatest.jl" begin
+    cache_1 = RegistryParsingCache()
+
     @testset "Unit tests" begin
         @testset "registry-list.jl" begin
             @test AbstractRegistry(LocalFolderRegistry("foo")) == LocalFolderRegistry("foo")
@@ -18,22 +20,27 @@ import UUIDs
                 Pkg.add(latest("Example"))
             end
             @testset "specify a cache" begin
-                cache = RegistryParsingCache()
+                
                 @testset "name only" begin
-                    Pkg.add(latest("Example"; cache = cache))
-                    Pkg.add(latest("Example"; cache = cache, registries = ["https://github.com/JuliaRegistries/General.git"]))
-                    Pkg.add([latest("Example"; cache = cache)])
-                    Pkg.add(AddLatest._convert_to_package_spec(latest("Example"; cache = cache)))
-                    Pkg.add(Pkg.Types.PackageSpec(latest("Example"; cache = cache)))
-                    Pkg.add(Pkg.API.Package(latest("Example"; cache = cache)))
-                    Pkg.add(AddLatest.convert(Pkg.Types.PackageSpec, latest("Example"; cache = cache)))
+                    Pkg.add(latest("Example"; cache = cache_1))
+                    Pkg.add(latest("Example"; cache = cache_1, registries = ["https://github.com/JuliaRegistries/General.git"]))
+                    Pkg.add([latest("Example"; cache = cache_1)])
+                    Pkg.add(AddLatest._convert_to_package_spec(latest("Example"; cache = cache_1)))
+                    Pkg.add(Pkg.Types.PackageSpec(latest("Example"; cache = cache_1)))
+                    Pkg.add(Pkg.API.Package(latest("Example"; cache = cache_1)))
+                    Pkg.add(AddLatest.convert(Pkg.Types.PackageSpec, latest("Example"; cache = cache_1)))
                 end
                 @testset "name and uuid" begin
-                    Pkg.add(latest("Example", UUIDs.UUID("7876af07-990d-54b4-ab0e-23690620f79a"); cache = cache))
+                    Pkg.add(latest("Example", UUIDs.UUID("7876af07-990d-54b4-ab0e-23690620f79a"); cache = cache_1))
                 end
             end
         end
+        @testset "test a package with a yanked version" begin 
+            latest("MLJBase"; cache = cache_1)
+        end
     end
+    
+    Base.empty!(cache)
 
     @testset "Doctests" begin
         Documenter.doctest(AddLatest)
